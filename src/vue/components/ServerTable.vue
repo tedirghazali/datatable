4576777ede7e1fd966b7ec061f9923c493fb2f94
Tedir Ghazali <tedirghazali@gmail.com>
2024-05-08 20:11:09 +0700

    fix search speed issue

--- src/vue/components/ServerTable.vue
+++ src/vue/components/ServerTable.vue
@@ -185,12 +185,12 @@ const filterHandler = (propVal: string) => {
     <slot name="header"></slot>
     <div class="dataTableHeader">
       <div class="group">
-        <span class="button groupItem">
+        <span class="button groupItem" @click="searchHandler">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="dataTableEvent bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
           </svg>
         </span>
-        <input type="search" ref="searchRef" @input="searchHandler" @keyup.enter="searchHandler" class="input groupItem">
+        <input type="search" ref="searchRef" @mouseout="searchHandler" @keyup.enter="searchHandler" class="input groupItem">
         <select v-if="filterBy === 'search'" v-model="searchBy" @change="searchHandler" class="select groupItem dataTableSearchBy">
           <option value="">{{ translate?.all || 'All' }}</option>
           <template v-for="(col, ind) in getColumns" :key="ind">
